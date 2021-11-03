4c4d2a99e2610b8b365974cfea0aca9df7d77e0b
igorpachp <igorpacheco9@gmail.com>
2021-11-03 19:34:17 -0300

    clear button and size slider

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# op-etch-a-sketch
+# Etch-a-Sketch
+
+Another one of [The Odin Project](https://www.theodinproject.com/home) exercercises, on this one the objective is to build a painting tool similar to a sketchpad, or an Etch-a-Sketch.
+
+## Tools to be implemented
+- Color Selection
+- Color Picker
+- Eraser
+- "Bucket" tool
--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
+const DEFAULT_CELL_COLOR = "#ffffff";
+
 const gridContainer = document.querySelector(".grid-container");
-const gridSize = 960;
+const resetButton = document.querySelector(".clear");
+const sizeSlider = document.querySelector(".size-slider");
+
+let brushMode = "mouseover";
 
 const colorListener = function (event) {
     event.currentTarget.style.backgroundColor = "blue";
 }
+
+const resetListener = function (event) {
+    gridContainer.innerHTML = "";
+    makeGrid(sizeSlider.value);
+}
+
+function updateTextInput(val) {
+    document.getElementById('textInput').value = val;
+}
+
+resetButton.addEventListener("click", resetListener, false);
 
 function makeGrid(size) {
     for(let i = 0; i < size*size; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
         cell.style.width = `${100/size}%`;
-        cell.addEventListener("mouseover", colorListener, false);
+        cell.addEventListener(brushMode, colorListener, false);
         gridContainer.appendChild(cell);
     }
 }
 
-makeGrid(16);
+makeGrid(sizeSlider.value);
