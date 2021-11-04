16de46614620798b9f6c8c2dcbd085021b3d0855
igorpachp <igorpacheco9@gmail.com>
2021-11-04 17:30:06 -0300

    organizing the code for future changes

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const brushButton = document.querySelector(".brush");
 const eraserButton = document.querySelector(".eraser");
 const rainbowButton = document.querySelector(".rainbow");
 const resizeButton = document.querySelector(".resize");
-const trailButton = document.querySelector(".trail");
+const trackButton = document.querySelector(".trail");
 const clickButton = document.querySelector(".click");
 
 // control variables
@@ -27,16 +27,35 @@ const colorListener = function (event) {
     event.currentTarget.style.backgroundColor = currentBrushColor === "random" ? randomizeColor() : currentBrushColor;
 }
 
+// GRID FUNCTIONS ---------------------------------------
 // resets grid with new size
 const resetListener = function () {
     gridContainer.innerHTML = "";
     makeGrid(currentSize);
 }
 
+function makeGrid(size) {
+    for(let i = 0; i < size*size; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = `${100/size}%`;
+        cell.style.backgroundColor = `#${DEFAULT_CELL_COLOR}`;
+        cell.addEventListener(currentBrushMode, colorListener, false);
+        gridContainer.appendChild(cell);
+    }
+}
+
+function resizeGrid() {
+    gridContainer.innerHTML = "";
+    makeGrid(sizeSlider.value);
+    currentSize = sizeSlider.value;
+}
+
 function updateTextOutput(val) {
     document.querySelector('.range-output').textContent = `${val} x ${val}`;
 }
 
+// BUTTONS FUNCTIONS -------------------------------------
 // handles wich button appears as active
 function toggleButton(target) {
     if (target.classList.contains("active")) {
@@ -45,6 +64,24 @@ function toggleButton(target) {
     else {
         target.classList.add("active");
     }
+}
+
+// deactivate all brush mode buttons
+function updateBrushButtons() {
+    if (brushButton.classList.contains("active"))
+        toggleButton(brushButton);
+    if (eraserButton.classList.contains("active"))
+        toggleButton(eraserButton);
+    if (rainbowButton.classList.contains("active"))
+        toggleButton(rainbowButton);
+}
+
+// deactivate all paint mode buttons
+function updatePaintButtons() {
+    if (clickButton.classList.contains("active"))
+        toggleButton(clickButton);
+    if (trackButton.classList.contains("active"))
+        toggleButton(trackButton);
 }
 
 // updates events for every cell in the grid
@@ -64,18 +101,19 @@ function toggleBrushMode(event) {
     if(target.classList.contains("click")) {
         if(!target.classList.contains("active")) {
             switchBrushEvents("click", currentBrushMode);
-            toggleButton(trailButton);
+            updatePaintButtons();
             toggleButton(target);
         }
         else {
             switchBrushEvents(DEFAULT_BRUSH_MODE, currentBrushMode);
-            toggleButton(target);
-            toggleButton(trailButton);
+            updatePaintButtons();
+            toggleButton(trackButton);
         }
     }
-    else {
+    else if (target.classList.contains("click")){
         if(!target.classList.contains("active")) {
             switchBrushEvents(DEFAULT_BRUSH_MODE, currentBrushMode);
+            updatePaintButtons();
             toggleButton(clickButton);
             toggleButton(target);
         }
@@ -87,19 +125,22 @@ function toggleBrushMode(event) {
     }
 }
 
-function updatePaintButtons() {
+function setCurrentColor(newColor) {
     if (brushButton.classList.contains("active"))
-        toggleButton(brushButton);
-    if (eraserButton.classList.contains("active"))
-        toggleButton(eraserButton);
-    if (rainbowButton.classList.contains("active"))
-        toggleButton(rainbowButton);
+        currentBrushColor = newColor;
+}
+
+function randomizeColor() {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    return `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
 function toggleBrush(event){
     target = event.currentTarget;
     if(!target.classList.contains("active")) {
-        updatePaintButtons();
+        updateBrushButtons();
         currentBrushColor = colorPicker.value;
         toggleButton(target);
     }
@@ -108,7 +149,7 @@ function toggleBrush(event){
 function toggleEraser(event) {
     target = event.currentTarget;
     if(!target.classList.contains("active")) {
-        updatePaintButtons();
+        updateBrushButtons();
         currentBrushColor = `#${DEFAULT_CELL_COLOR}`;
         toggleButton(target);
     }
@@ -119,17 +160,10 @@ function toggleEraser(event) {
     }
 }
 
-function randomizeColor() {
-    const randomR = Math.floor(Math.random() * 256);
-    const randomG = Math.floor(Math.random() * 256);
-    const randomB = Math.floor(Math.random() * 256);
-    return `rgb(${randomR}, ${randomG}, ${randomB})`;
-}
-
 function toggleRainbow(event) {
     target = event.currentTarget;
     if(!target.classList.contains("active")) {
-        updatePaintButtons();
+        updateBrushButtons();
         currentBrushColor = "random";
         toggleButton(target);
     }
@@ -140,35 +174,14 @@ function toggleRainbow(event) {
     }
 }
 
-function setCurrentColor(newColor) {
-    if (brushButton.classList.contains("active"))
-        currentBrushColor = newColor;
-}
-
 // event listeners for each individual task
 resetButton.addEventListener("click", resetListener, false);
 brushButton.addEventListener("click", toggleBrush, false);
 eraserButton.addEventListener("click", toggleEraser, false);
 rainbowButton.addEventListener("click", toggleRainbow, false);
 resizeButton.addEventListener("click", resizeGrid, false);
-trailButton.addEventListener("click", toggleBrushMode, false);
+trackButton.addEventListener("click", toggleBrushMode, false);
 clickButton.addEventListener("click", toggleBrushMode, false);
 colorPicker.onchange = (e) => setCurrentColor(colorPicker.value);
-function makeGrid(size) {
-    for(let i = 0; i < size*size; i++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.style.width = `${100/size}%`;
-        cell.style.backgroundColor = `#${DEFAULT_CELL_COLOR}`;
-        cell.addEventListener(currentBrushMode, colorListener, false);
-        gridContainer.appendChild(cell);
-    }
-}
-
-function resizeGrid() {
-    gridContainer.innerHTML = "";
-    makeGrid(sizeSlider.value);
-    currentSize = sizeSlider.value;
-}
 
 makeGrid(sizeSlider.value);
